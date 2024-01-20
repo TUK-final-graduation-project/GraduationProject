02328b38ad6f14dc6edf49369774cddaf5a1cb70
Gahyun <elfgh29@naver.com>
2024-01-20 22:04:42 +0900

    Astar cpp 프로젝트 생성

---
+++ CppProject/Astar.cpp
@@ -0,0 +1,7 @@
+#include "Astar.h"
+
+extern "C" {
+	int DLL_EXPORT SimpleReturnFun() {
+		return 20;
+	}
+}
---
+++ CppProject/Astar.h
@@ -0,0 +1,10 @@
+#ifndef ASTAR_H
+#define ASTAR_H
+
+#define DLL_EXPORT __declspec(dllexport)
+
+extern "C" {
+	int DLL_EXPORT SimpleReturnFun();
+}
+
+#endif
