0fa28f2e926a808d0bab78a16248b60745c0a0a6
Lee Seung Hee <senughee521@gmail.com>
2024-03-29 17:16:39 +0900

    플레이어 맵 재설정

---
+++ DO_NOT_DISTURB/Assets/Script/Player/Slicer/Sliceable.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine;
+
+public class Sliceable : MonoBehaviour
+{
+    [SerializeField]
+    private bool isSolid = true;
+
+    [SerializeField]
+    private bool reverseWindTriangles = false;
+
+    [SerializeField]
+    private bool useGravity = false;
+
+    [SerializeField]
+    private bool shareVertices = false;
+
+    [SerializeField]
+    private bool smoothVertices = false;
+
+    public bool IsSolid
+    {
+        get
+        {
+            return isSolid;
+        }
+        set
+        {
+            isSolid = value;
+        }
+    }
+
+    public bool ReverseWireTriangles
+    {
+        get
+        {
+            return reverseWindTriangles;
+        }
+        set
+        {
+            reverseWindTriangles = value;
+        }
+    }
+
+    public bool UseGravity
+    {
+        get
+        {
+            return useGravity;
+        }
+        set
+        {
+            useGravity = value;
+        }
+    }
+
+    public bool ShareVertices
+    {
+        get
+        {
+            return shareVertices;
+        }
+        set
+        {
+            shareVertices = value;
+        }
+    }
+
+    public bool SmoothVertices
+    {
+        get
+        {
+            return smoothVertices;
+        }
+        set
+        {
+            smoothVertices = value;
+        }
+    }
+
+}
