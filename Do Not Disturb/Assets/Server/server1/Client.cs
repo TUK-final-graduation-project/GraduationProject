e0caf7d20a8cdabc5e32e17dfa4be98069338ca2
Seunghee-030 <73768560+Seunghee-030@users.noreply.github.com>
2024-07-14 00:46:45 +0900

    플레이어 수정

--- Do Not Disturb/Assets/Server/server1/Client.cs
+++ Do Not Disturb/Assets/Server/server1/Client.cs
@@ -17,7 +17,7 @@ public class Client : MonoBehaviour
     NetworkStream stream;
     StreamWriter writer;
     StreamReader reader;
-    Player_Movement player;
+    PlayerMovement player;
     AnotherPlayer anotherPlayer;
 
     public Vector3 playerPosition;
@@ -25,7 +25,7 @@ public class Client : MonoBehaviour
     void Start()
     {
         // Player_Movement 스크립트를 가지고 있는 게임 오브젝트를 찾습니다.
-        player = FindObjectOfType<Player_Movement>();
+        player = FindObjectOfType<PlayerMovement>();
         anotherPlayer = FindObjectOfType<AnotherPlayer>();
     }
 
