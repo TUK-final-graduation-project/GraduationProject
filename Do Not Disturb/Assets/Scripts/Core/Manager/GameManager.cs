70bab0682802f835cba54f295bfb3c5584875c1b
Seunghee-030 <senughee521@gmail.com>
2024-07-23 14:42:14 +0900

    아이템 아이콘 변경

--- Do Not Disturb/Assets/Scripts/Core/Manager/GameManager.cs
+++ Do Not Disturb/Assets/Scripts/Core/Manager/GameManager.cs
@@ -149,7 +149,7 @@ public class GameManager : MonoBehaviour
 
         if (playerMovement != null)
         {
-            coinTxt.text = "Coins: " + playerMovement.coinCount.ToString();
+            coinTxt.text = playerMovement.coinCount.ToString() + " $";
             laboratory.UpdateButtonUI();
         }
     }
