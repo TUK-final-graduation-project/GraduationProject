47dd80633d925a213a4bf0df3a76ceda8fa608d4
Gahyun <elfgh29@naver.com>
2024-07-24 20:23:23 +0900

    유저 베이스 변경 및 연구소 메쉬 설정

--- Do Not Disturb/Assets/Scripts/Boss/PlayerAttackToBoss.cs
+++ Do Not Disturb/Assets/Scripts/Boss/PlayerAttackToBoss.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VictoryBoss : MonoBehaviour
+public class PlayerAttackToBoss : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
