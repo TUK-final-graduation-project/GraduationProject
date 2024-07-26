854fdee29af27d01d690e03adf23f526495996e3
Seunghee-030 <senughee521@gmail.com>
2024-07-27 04:46:42 +0900

    플레이어 정보

--- Do Not Disturb/Assets/Scripts/Photon/PhotonManager.cs
+++ Do Not Disturb/Assets/Scripts/Photon/PhotonManager.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using Unity.VisualScripting;
+
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
     // 버전 입력
     private readonly string version = "1.0f";
     // 사용자 아이디 입력
     private string userID = "Bony";
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -25,30 +25,27 @@ public class PhotonManager : MonoBehaviourPunCallbacks
         Debug.Log(PhotonNetwork.SendRate);
         // 서버 접속
         PhotonNetwork.ConnectUsingSettings();
-               
     }
 
-    //  포톤 서버에 접속 후 호출되는 콜백 함수
+    // 포톤 서버에 접속 후 호출되는 콜백 함수
     public override void OnConnectedToMaster()
     {
-        //base.OnConnectedToMaster();
         Debug.Log("Connected To Master");
         Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
         PhotonNetwork.JoinLobby();
-
     }
 
-    //  로비에 접속 후 호출되는 콜백 함수
+    // 로비에 접속 후 호출되는 콜백 함수
     public override void OnJoinedLobby()
     {
         Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
-        PhotonNetwork.JoinRandomRoom();         // 랜덤 매치메이킹 기능
+        PhotonNetwork.JoinRandomRoom();  // 랜덤 매치메이킹 기능
     }
 
     // 랜덤한 룸 입장이 실패했을 경우 호출되는 콜백 함수
     public override void OnJoinRandomFailed(short returnCode, string message)
     {
-        Debug.Log($"JoinRandom Filed {returnCode}:{message}");
+        Debug.Log($"JoinRandom Failed {returnCode}:{message}");
 
         // 룸의 속성 정의
         RoomOptions ro = new RoomOptions();
@@ -61,23 +58,28 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     }
 
     // 룸 생성이 완료된 후 호출되는 콜백 함수
-    public override void OnConnected()
+    public override void OnCreatedRoom()
     {
-        Debug.Log("Create Room");
+        Debug.Log("Room Created");
         Debug.Log($"Room Name = {PhotonNetwork.CurrentRoom.Name}");
     }
 
     // 룸에 입장한 후 호출되는 콜백 함수
     public override void OnJoinedRoom()
     {
-        Debug.Log($"PhotonNetWork.InRoom = {PhotonNetwork.InRoom}");
+        Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}");
         Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}");
-
+        
+        // 룸에 접속한 사용자 정보 확인
+        foreach(var player in PhotonNetwork.CurrentRoom.Players)
+        {
+            Debug.Log($"{player.Value.NickName}.{player.Value.ActorNumber}");
+            // $ => String.Format()
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
