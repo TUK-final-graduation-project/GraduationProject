b3439bdf055e9b908e6f3c4fbe6d356ac3ba7ec7
GH_NoteBook <elfgh29@naver.com>
2024-07-27 18:34:53 +0900

    test scene 스크립트 정리

--- Do Not Disturb/Assets/Scripts/Tower/Tower/TestSceneManager.cs
+++ Do Not Disturb/Assets/Scripts/Tower/Tower/TestSceneManager.cs
@@ -6,54 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class TestSceneManager : MonoBehaviour
 {
-    public bool isSpawnUnit;
-    public bool isAstar = false;
-    public bool vDown;
-    public bool cDown;
-    bool xDown;
+
     bool bDown;
     bool nDown;
     bool mDown;
     bool escDown;
-
-    public ComSpawnPoint fireLandMark;
-    public GameObject obs;
 
     int lobbyScene = 0;
     int gameScene = 1;
     int optionScene = 2;
     void Update()
     {
-        vDown = Input.GetButton("Debug SpawnUnit");
-        cDown = Input.GetButton("Debug Astar");
-        xDown = Input.GetButton("Debug offObs");
         bDown = Input.GetButton("Game Scene");
         nDown = Input.GetButton("Mini Scene");
         mDown = Input.GetButton("Lobby Scene");
         escDown = Input.GetButton("Game End");
 
-        if (vDown && !isSpawnUnit)
-        {
-            Debug.Log("press v button");
-            fireLandMark.StartSpawn(1);
-            isSpawnUnit = true;
-
-            Invoke("SpawnCoolTime", 3);
-        }
-        if ( cDown && !isAstar )
-        {
-            Debug.Log("on c button");
-            isAstar = true;
-            
-        }
-        if (xDown)
-        {
-            obs.SetActive(false);
-        }
-        if (cDown)
-        {
-            obs.SetActive(true);
-        }
         if (bDown)
         {
             SceneManager.LoadScene(gameScene);
@@ -88,11 +56,5 @@ public class TestSceneManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = true;
         SceneManager.LoadScene(lobbyScene);
-    }
-    
-
-    void SpawnCoolTime()
-    {
-        isSpawnUnit = false;
     }
 }
